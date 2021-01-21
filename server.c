71b7e22c572d82e0fb1abe81cc8762e5b983dfd5
Ardhi Putra Pratama <me@ardhipoetra.web.id>
2021-01-21 15:16:53 +0100

    Update server.c example - with emmc
    add minimal documentation on how to use emmc

--- server.c
+++ server.c
@@ -5,12 +5,18 @@
 #include <dqlite.h>
 
 int main() {
+  // dqlite interface to talk with client
   const char *address = "172.17.0.5:5007";
+  
+  // dqlite/raft directory
   const char *dir = "/tmp/dqlite-rs7";
   dqlite_node *node;
   int rv;
   mkdir(dir, 0755);
-  rv = dqlite_node_create(3, address, dir, &node);
+  // with default emmc (127.0.0.1 : 8889)
+  // rv = dqlite_node_create(3, address, dir, &node);
+  // with emmc address : .0.4:50006
+  rv = dqlite_node_create_wemmc(3, address, dir, &node, "172.17.0.4", 50006); 
   if (rv != 0) {
     printf("dqlite_node_create: %d\n", rv);
   }
